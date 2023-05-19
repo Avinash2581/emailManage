e615fb0a0422986671ccb79209197c3b9c18bf6e
Avinash2581 <sahuavinash91141@gmail.com>
2023-05-19 12:48:51 +0530

    made some changes

---
+++ src/main/java/com/kloc/emailApp/entity/Changes.java
@@ -0,0 +1,11 @@
+package com.kloc.emailApp.entity;
+
+import lombok.Data;
+
+@Data
+public class Changes {
+	private String gitUserName;
+	private String gitUserPassword;
+	private String message;
+
+}
